5b464f81b8ad05de96431c92277379fc72929c1d
Ahmed Hamdy <ahmedhamdy1515@hotmail.com>
2025-11-17 18:21:59 +0200

    i make file .gitignore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import { config } from 'dotenv'
+config();
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
